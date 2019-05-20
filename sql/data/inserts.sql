f239cfc0e2aaa4c4860689380935079fc3185d4f
spyrospav <spyrospavlatos4@gmail.com>
2019-05-20 15:26:25 +0300

    more inserts

--- sql/data/inserts.sql
+++ sql/data/inserts.sql
@@ -4,12 +4,15 @@ INSERT INTO library.member(MFirst, MLast, Street, Street_num, Postal_code, MBirt
 
 INSERT INTO library.publisher(pubName, estYear, Street, Street_num, Postal_code) VALUES('Fountas', 1995, 'Athinas', 47, 11177);
 INSERT INTO library.publisher(pubName, estYear, Street, Street_num, Postal_code) VALUES('Tziolas', 1995, 'Akadimias', 12, 11174);
-
+INSERT INTO library.publisher(pubName, estYear, Street, Street_num, Postal_code) VALUES('Simmetria', 1980, 'Zografou', 33, 11354);
 
 INSERT INTO library.book(ISBN, title, pubYear, numPages, pubName) VALUES(9789603307, 'Digital Signal Proccessing', 1999, 978, 'Fountas');
 INSERT INTO library.book(ISBN, title, pubYear, numPages, pubName) VALUES(9710603307, 'Signals and Systems', 1995, 1015, 'Fountas');
 INSERT INTO library.book(ISBN, title, pubYear, numPages, pubName) VALUES(9712647888, 'Circuit Analysis', 1990, 568, 'Fountas');
+INSERT INTO library.book(ISBN, title, pubYear, numPages, pubName) VALUES(9730553210, 'Mathematical Analysis I', 2003, 1235, 'Simmetria');
 
 INSERT INTO library.author(AFirst, ALast, ABirthdate) VALUES('Alan','Oppenheim','1937-11-7');
+INSERT INTO library.author(AFirst, ALast, ABirthdate) VALUES('Themistoklis','Rassias','1951-4-2');
 
 INSERT INTO library.written_by(ISBN, authID) VALUES(9789603307,1);
+INSERT INTO library.written_by(ISBN, authID) VALUES(9730553210,2);
