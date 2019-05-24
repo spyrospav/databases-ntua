97ee83fca39e81a625bb5d70e327a3bc590f6997
Chariton Charitonidis <haritonch@yahoo.com>
2019-05-24 20:48:20 +0300

    Add files via upload

---
+++ my_app/src/Member/SearchForm.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+class SearchForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      option: '', //title, author, category
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+  handleClick(option) {
+    this.setState({
+      option: option
+    })
+  }
+  handleSubmit(event) {
+    //socket.emit(...)
+    console.log(this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+    <div class="searchform">
+      <form onSubmit={this.handleSubmit}>
+        <div class="search-by">
+            <h3>Search</h3>
+            <div class="radiotext">
+                <p> Title </p>
+                <input type="radio" name="searchOption" onClick={() => this.handleClick("title")}/>
+            </div>
+            <div class="radiotext">
+                <p> Author </p>
+                <input type="radio" name="searchOption" onClick={() => this.handleClick("author")}/>
+            </div>
+            <div class="radiotext">
+                <p> Category </p>
+                <input type="radio" name="searchOption" onClick={() => this.handleClick("category")}/> <br/>
+            </div>
+        </div>
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            <input type="submit" value="Search" />
+      </form>
+    </div>
+    );
+  }
+}
+
+export { SearchForm };
