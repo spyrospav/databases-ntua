6c56571a21e01dadd79d729f9aa2067b4fc1573f
Chariton Charitonidis <haritonch@yahoo.com>
2019-05-25 17:57:20 +0300

    small enhancements

--- my_app/src/Welcome/welcome.js
+++ my_app/src/Welcome/welcome.js
@@ -4,11 +4,12 @@ import Button from '@material-ui/core/Button';
 const Welcome = ( { handleChangeStatus } ) => {
   return (
   <div>
-    <Button variant="outlined" onClick={() => handleChangeStatus('signIn', 'member')}> Login as Member</Button>
+    <h4> Welcome to our Library, if you don't have an account please Sign Up</h4>
+    <Button class="btn" variant="outlined" onClick={() => handleChangeStatus('signIn', 'member')}> Login as Member</Button>
     <br/>
-    <Button variant="outlined" onClick={() => handleChangeStatus('signIn', 'employee')}> Login as Employee </Button>
+    <Button class="btn" variant="outlined" onClick={() => handleChangeStatus('signIn', 'employee')}> Login as Employee </Button>
     <br/>
-    <Button variant="outlined" onClick={() => handleChangeStatus('signUp')}> Sign up </Button>
+    <Button class="btn" variant="outlined" onClick={() => handleChangeStatus('signUp')}> Sign up </Button>
     <br/>
   </div>
 );
