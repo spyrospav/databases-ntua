558b14ca7117ee12075da955628aa78af77b5624
Chariton Charitonidis <haritonch@yahoo.com>
2019-05-27 16:51:44 +0300

    styled ShowBookArray

--- my_app/src/Employee/Employee.js
+++ my_app/src/Employee/Employee.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { SearchForm } from './SearchForm';
-import { ShowBookArray } from './ShowBookArray';
+import React from "react";
+import { SearchForm } from "./SearchForm";
+import { ShowBookArray } from "./ShowBookArray";
 
 const Employee = ({
   foundBooks,
@@ -9,58 +9,63 @@ const Employee = ({
   handleChangeState,
   handleAddEmployee,
   handleSendReminder
-  }) => {
+}) => {
   return (
-    <div>
-      <div className="left-div content-box">
-        <SearchForm />
-        <div className="results">
-          <h2>Results</h2>
-          {foundBooks.map(book =>
-          <div key={book.title+book.author} className="book">
-            <div>
-              <p> {book.title}, {book.author} </p>
-            </div>
-          </div>
-          )}
-        </div>
-      </div>
+//    <div>
+//      <div className="left-div content-box">
+//        <SearchForm />
+//        <div className="results">
+//          <h2>Results</h2>
+//          {foundBooks.map(book => (
+//            <div key={book.title + book.author} className="book">
+//              <div>
+//                <p>
+//                  {" "}
+//                  {book.title}, {book.author}{" "}
+//                </p>
+//              </div>
+//            </div>
+//          ))}
+//        </div>
+//      </div>
+//
+//      <div className="middle-div content-box">
+//        <h2> Expired: </h2>
+//        {expiredBooks.map(book => (
+//          <div key={book.memberID + book.ISBN}>
+//            <p> Member ID: {book.memberID}</p>
+//            <p> ISBN: {book.ISBN}</p>
+//            <p> Copy Number: {book.copyNumber}</p>
+//            <p> Date of Borrowing: {book.dateOfBorrowing} </p>
+//            <p> Due to: {book.dueDate} </p>
+//            <button onClick={() => handleSendReminder()}>
+//              {" "}
+//              Send reminder{" "}
+//            </button>
+//          </div>
+//        ))}
+//      </div>
+//
+//      <h2> Add new employee </h2>
+//      <button onClick={() => handleAddEmployee()}> Add </button>
+//
+//      <form>
+//        <div className="radiotext">
+//          <p> Title </p>
+//          <input type="radio" name="searchOption" />
+//        </div>
+//        <div className="radiotext">
+//          <p> Author </p>
+//          <input type="radio" name="searchOption" />
+//        </div>
+//        <div className="radiotext">
+//          <p> Category </p>
+//          <input type="radio" name="searchOption" /> <br />
+//        </div>
+//      </form>
 
-      <div className="middle-div content-box">
-        <h2> Expired: </h2>
-        {expiredBooks.map(book =>
-          <div key={book.memberID+book.ISBN}>
-            <p> Member ID: {book.memberID}</p>
-            <p> ISBN: {book.ISBN}</p>
-            <p> Copy Number: {book.copyNumber}</p>
-            <p> Date of Borrowing: {book.dateOfBorrowing} </p>
-            <p> Due to: {book.dueDate} </p>
-            <button onClick={() => handleSendReminder()}> Send reminder </button>
-          </div>
-        )}
-      </div>
-
-      <h2> Add new employee </h2>
-      <button onClick={() => handleAddEmployee()}> Add </button>
-
-      <form>
-        <div className="radiotext">
-            <p> Title </p>
-            <input type="radio" name="searchOption"/>
-        </div>
-        <div className="radiotext">
-            <p> Author </p>
-            <input type="radio" name="searchOption"/>
-        </div>
-        <div className="radiotext">
-            <p> Category </p>
-            <input type="radio" name="searchOption"/> <br/>
-        </div>
-      </form>
-
-      <ShowBookArray booksArray={booksArray}/>
-
-    </div>
+      <ShowBookArray booksArray={booksArray} />
+//    </div>
   );
 };
 
--- my_app/src/Employee/ShowBookArray.js
+++ my_app/src/Employee/ShowBookArray.js
@@ -1,75 +1,83 @@
-import React from 'react';
-import { InsertFields } from './InsertFields';
-import { EditFields } from './EditFields';
+import React from "react";
+import { InsertFields } from "./InsertFields";
+import { EditFields } from "./EditFields";
 
 class ShowBookArray extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      status: '', //'insert', 'edit', 'delete'
+      status: "", //'insert', 'edit', 'delete'
       indexEdit: -1
     };
-
   }
   render() {
     return (
       <div className="books">
         <h2> Books </h2>
-        {this.state.status === 'insert'
-          ?
+        {this.state.status === "insert" ? (
           <div>
             <InsertFields />
-            <button onClick={() => this.setState({status:''})}>
-            Undo
-            </button>
-            <button> Insert </button>
+            <button className="btn" onClick={() => this.setState({ status: "" })}>Undo</button>
+            <button className="btn"> Insert </button>
           </div>
-          :
-        <button onClick={() => this.setState({status:'insert'})}>
-          Insert book
-        </button>
-        }
+        ) : (
+          <button className="btn" onClick={() => this.setState({ status: "insert" })}>
+            Insert book
+          </button>
+        )}
+        <div className="row6 bold">
+                <p className="col6">ISBN</p>
+                <p className="col6">Title</p>
+                <p className="col6">Author</p>
+                <p className="col6">Publisher</p>
+                <p className="col6">Published</p>
+                <p className="col6">Pages</p>
+                <p className="col6">Avail. Copies</p>
+        </div>
 
         {this.props.booksArray.map((book, index) => {
           if (this.state.indexEdit === index) {
-              return (
-                <div key={book.ISBN}>
-                  <EditFields
+            return (
+              <div key={book.ISBN}>
+                <EditFields
                   key={book.ISBN}
                   ISBN={book.ISBN}
                   title={book.title}
-                  author={book.author} 
+                  author={book.author}
                   publisher={book.publisher}
                   publicationYear={book.publicationYear}
                   numOfPages={book.numOfPages}
                   numOfCopies={book.numOfCopies}
-                  />
-                  <button> Update </button>
-                </div>
-              );
-          }
-          else {
+                />
+                <button> Update </button>
+              </div>
+            );
+          } else {
             return (
-            <div className="book" key={book.ISBN}>
-              <p className="col6">{ book.ISBN }</p>
-              <p className="col6">{ book.title }</p>
-              <p className="col6">{ book.author }</p>
-              <p className="col6">{ book.publisher }</p>
-              <p className="col6">{ book.publicationYear }</p>
-              <p className="col6">{ book.numOfPages }</p>
-              <p className="col6">{ book.numOfCopies }</p>
-              <button className="btn-small"
-              onClick={() => this.setState({status:'edit', indexEdit: index})}>
-                Edit
-              </button>
-              <button className="btn-small"> Delete </button>
-            </div>
-          );
-          }}
-        )}
-        </div>
-      )}
+              <div className="row6" key={book.ISBN}>
+                <p className="col6">{book.ISBN}</p>
+                <p className="col6">{book.title}</p>
+                <p className="col6">{book.author}</p>
+                <p className="col6">{book.publisher}</p>
+                <p className="col6">{book.publicationYear}</p>
+                <p className="col6">{book.numOfPages}</p>
+                <p className="col6">{book.numOfCopies}</p>
+                <button
+                  className="btn-small"
+                  onClick={() =>
+                    this.setState({ status: "edit", indexEdit: index })
+                  }
+                >
+                  Edit
+                </button>
+                <button className="btn-small">Delete</button>
+              </div>
+            );
+          }
+        })}
+      </div>
+    );
+  }
 }
-
 
 export { ShowBookArray };
